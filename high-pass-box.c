488f66f6730e82bd10d3f427b1c85b43b366e6f0
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2022-05-08 09:44:40 +0000

    Add files via upload

--- high-pass-box.c
+++ high-pass-box.c
@@ -84,7 +84,7 @@ gegl_op_class_init (GeglOpClass *klass)
                                  "reference-hash", "x62261a5feb6c696f76cb80e68f4930e2",
                                  "reference-hashB", "xcf04bb714a173f1f71da8af283b8a894",
                                  "description",
-                                 _("Enhances fine details using a reversal of Box Blur's algorithm'."),
+                                 _("Enhances fine details using a reversal of Box Blur's algorithm. Blend modes Linear Light and Grain Merge make it sharpen."),
                                  NULL);
 }
 
